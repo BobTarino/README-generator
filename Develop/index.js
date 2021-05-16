dd5bfc420a4c29947251527e81885df87115832a
Bobby T <bob.tarino@gmail.com>
2021-05-16 15:25:48 -0500

    created fuctions for license link and badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,85 +1,89 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkDown = require('./utils/generateMarkdown');
+const path = require("path");
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {   type: "input", 
-        name: "title",
-        message: "What is the name of your project?"
-    }, 
-    {
-        type: "list",
-        name: "license",
-        message: "Choose the license you used for your project:",
-        choices: ["MIT", "apache", "eclipse", "PostgreSQL", "none"]
-    },
+const questions = () => {
+return inquirer
+    .prompt([
 
-    {
-        type: "input",
-        name: "description",
-        message: "Write a description for your project."
-    },
+        {   type: "input", 
+            name: "title",
+            message: "What is the name of your project?"
+        }, 
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the license you used for your project:",
+            choices: ["MIT", "apache", "eclipse", "PostgreSQL", "none"]
+        },
 
-    {
-        type: "input",
-        name: "installation",
-        message: "Please provide the  steps required for installation:",
+        {
+            type: "input",
+            name: "description",
+            message: "Write a description for your project."
+        },
+
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide the  steps required for installation:",
+            
+        },
+
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide instructions and examples for use:"
+        },
+
+        {
+            type: "input",
+            name: "contributing",
+            message: "Please provide guidelines for other developers to contribute:"
+            
+        },
+
+        {
+            type: "input",
+            name: "test",
+            message: "Please provide tests for your application?",
         
-    },
+        },
 
-    {
-        type: "input",
-        name: "usage",
-        message: "Please provide instructions and examples for use:"
-    },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
 
-    {
-        type: "input",
-        name: "contributing",
-        message: "Please provide guidelines for other developers to contribute:"
-        
-    },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?"
+        },
+    
+    ])
 
-    {
-        type: "input",
-        name: "test",
-        message: "Please provide tests for your application?",
-       
-    },
-
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    },
-
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?"
-    },
    
 
-
-
-
-
-
-
-
-
-
-
-
-
-];
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// creating path for __dirname (created directory) and fileName
+function writeToFile(fileName, data){
+    return fs.writeFile(path.join(__dirname, fileName), data, err => {
+        console.log(err);
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then(response => {
+            writeToFile("README.md", generateMarkdown(response));
+            console.log("Success!")
+        });
+};
 
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function licenseBadge(license){
+  if (license !== "none"){
+    return `
+    ![badgeImage](https://img.shields.io/badge/license-${license}-blue)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function licenseLink(license) {
+  if (license !== "none") {
+    return `* [License](#license)`
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+    Licensed under ${license}`
+  }
+
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ${licenseBadge(data.license)}
+    ## Description
+    ${data.description}
+    \`\`\`
+    ## Table of contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    ${licenseLink(data.license)}
+    *[Contributing](#contributing)
+    *[Test](#test)
+    ## Installation
+    To install dependancies, run the following commands:
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+    ## Usage
+    ${data.usage}
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+    ${data.contributing}
+    ## Test
+    \`\`\`
+    ${data.test}
+    \`\`\`
+    ## Questions
+    If you have questions about this repository, please contact me at ${data.email}
+    You can see any of my other work at [${data.github}](https://github.com/${data.github}/)
+    `;
 }
 
 module.exports = generateMarkdown;
